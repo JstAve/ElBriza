4886f318782333077a23332e901c6a755decffc4
Dorothea Avellana <avellanadorothea@gmail.com>
2025-01-03 00:16:25 +0800

    directory link

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/elbriza/css/common.css">
+  <link rel="stylesheet" href="https://bclma.io/elbriza/css/common.css">
+
 
 
 
